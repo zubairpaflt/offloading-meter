5fafcd78248ea90b646d779757ac5436f02f00b2
Muhammad Zubair <zubairpaflt@gmail.com>
2026-05-15 18:16:34 +0500

    remove npmrc script-shell issue

--- node_modules/@modelcontextprotocol/ext-apps/dist/src/react/useAutoResize.examples.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Type-checked examples for the useAutoResize hook.
- *
- * @module
- */
-export {};
--- node_modules/@modelcontextprotocol/ext-apps/dist/src/react/useHostStyles.examples.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Type-checked examples for the useHostStyles hooks.
- *
- * @module
- */
-export {};
